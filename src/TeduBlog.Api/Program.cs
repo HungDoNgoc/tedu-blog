0bbffc06c7dd5ea61610701316ba5586830a0a98
ASUS <hungdo2706@gmail.com>
2024-01-21 04:27:09 +0700

    2.7.Migrate database version 1

--- src/TeduBlog.Api/Program.cs
+++ src/TeduBlog.Api/Program.cs
@@ -1,7 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TeduBlog.Core.Domain.Identity;
+using TeduBlog.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
 
+//Confit DB Context and ASP.NET Core Identity
+builder.Services.AddDbContext<TeduBlogContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<TeduBlogContext>();
+builder.Services.Configure<IdentityOptions>(Options =>
+{
+    // Password setting.
+    Options.Password.RequireDigit = true;
+    Options.Password.RequireLowercase = true;
+    Options.Password.RequireNonAlphanumeric = true;
+    Options.Password.RequireUppercase = true;
+    Options.Password.RequiredLength = 6;
+    Options.Password.RequiredUniqueChars = 1;
+
+    // Lockout setting.
+    Options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    Options.Lockout.MaxFailedAccessAttempts = 5;
+    Options.Lockout.AllowedForNewUsers = true;
+
+    // User setting.
+    Options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    Options.User.RequireUniqueEmail = false;
+});
+
+//Default config for ASP.NET core
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ src/TeduBlog.Data/TeduBlogContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduBlog.Data
+{
+    public class TeduBlogContextFactory : IDesignTimeDbContextFactory<TeduBlogContext>
+    {
+        public TeduBlogContext CreateDbContext(string[] args)
+        {
+            var configurations = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            var builder = new DbContextOptionsBuilder<TeduBlogContext>();
+            builder.UseSqlServer(configurations.GetConnectionString("DefaultConnection"));
+            return new TeduBlogContext(builder.Options);
+        }
+    }
+}
